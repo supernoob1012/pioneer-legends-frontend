f56930d154655d6a7bb83251025f269f60c14dea
supernoob1012 <felixmartel.no@gmail.com>
2023-10-28 16:54:25 +0900

    feat: fix response

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -29,6 +29,24 @@ const Map = () => {
   const content = useRef<HTMLDivElement>(null);
   const video = useRef<HTMLVideoElement>(null);
   const { setIsStakeModal } = useContext<any>(ModalContext);
+  const [zoom, setZoom] = useState(1);
+  const [zoomRate, setZoomRate] = useState(0);
+  const [viewWidth, setViewWidth] = useState(0);
+  const [viewHeight, setViewHeight] = useState(0);
+
+  const zoomIn = useCallback(() => {
+    if (zoom < 1.25) {
+      setZoom(zoom + 0.05);
+      setZoomRate(zoomRate + 1);
+    }
+  }, [zoom, zoomRate]);
+
+  const zoomOut = useCallback(() => {
+    if (zoom > 1) {
+      setZoom(zoom - 0.05);
+      setZoomRate(zoomRate - 1);
+    }
+  }, [zoom, zoomRate]);
 
   const wallet = useWallet();
 
@@ -43,19 +61,32 @@ const Map = () => {
         scrollMode: "transform",
         direction: "all",
         emulateScroll: true,
+
         bounce: false, // Adds a bounce effect when content edge is reached
       });
-
       const offsetX =
         content.current!.scrollWidth - viewport.current!.offsetWidth;
       const offsetY =
-        content.current!.scrollHeight - viewport.current!.offsetHeight!;
+        video.current!.scrollHeight - viewport.current!.offsetHeight!;
       sb.setPosition({
         x: offsetX / 2,
         y: offsetY / 2,
       });
     }
-  }, [viewport, content.current, video.current]);
+  }, [viewport]);
+
+  useEffect(() => {
+    if (width * 9 === height * 16) {
+      setViewHeight(height);
+      setViewWidth(width)
+    } else if (width * 9 > height * 16) {
+      setViewHeight(width * 9 / 16);
+      setViewWidth(width)
+    } else {
+      setViewHeight(height);
+      setViewWidth(height * 16 / 9)
+    }
+  }, [width, height])
 
   return (
     <>
@@ -91,17 +122,23 @@ const Map = () => {
                 />
               </div>
             </Link>
+            {/* {console.log('test', height, width)} */}
             <div
+            className=""
               ref={content}
-              className="relative"
               style={{
-                minWidth: "100vw",
-                height: "auto",
+                width: viewWidth,
+                height: viewHeight,
+                // width: (3840 / 2160) * (height + (width > 768 ? 100 : 300)),
+                // height: height + 300,
               }}
             >
               <video
                 ref={video}
-                className="absolute w-screen h-auto z-20 object-cover object-left-top"
+                className="w-full h-full z-20 object-cover object-left-top"
+                style={{
+                  transform: `scale(${zoom})`
+                }}
                 autoPlay={true}
                 playsInline
                 loop
@@ -116,18 +153,17 @@ const Map = () => {
                   data-wf-ignore="true"
                 />
               </video>
-              {/* <div className="absolute left-0 top-0 w-full h-full z-20">
+              <div className="absolute left-0 top-0 w-full h-full z-20">
                 <TitleBox
                   title="airship"
                   icon={<SpaceshipIcon />}
                   balance={10000}
                   supply={1024.59}
-                  left={317}
-                  top={424}
-                  hoverLeft={-62}
-                  hoverTop={-325}
-                  width={300}
-                  height={300}
+                  left={(-792 - 70) * zoomRate}
+                  top={-184 - 20 * zoomRate}
+                  hoverLeft={-124 + (0 * zoomRate)}
+                  hoverTop={-370 + (0 * zoomRate)}
+                  zoomRate={zoom}
                   onClick={handleOpenSpaceship}
                   isBottom
                 />
@@ -136,37 +172,56 @@ const Map = () => {
                   icon={<TownhallIcon />}
                   balance={10000}
                   supply={1024.59}
-                  left={876}
-                  top={310}
-                  hoverLeft={-58}
-                  hoverTop={119}
-                  width={300}
-                  height={300}
+                  left={-105 - 2 * zoomRate}
+                  top={-300 - 22 * zoomRate}
+                  hoverLeft={-120 + (0 * zoomRate)}
+                  hoverTop={158 + (0 * zoomRate)}
+                  zoomRate={zoom}
                   onClick={handleOpenSpaceship}
-                  isBottom
                 />
                 <TitleBox
                   title="mining"
                   icon={<MiningIcon />}
                   balance={10000}
                   supply={1024.59}
-                  left={1302}
-                  top={376}
-                  hoverLeft={-64}
-                  hoverTop={41}
-                  width={300}
-                  height={300}
+                  left={410 + 49 * zoomRate}
+                  top={-220 - 14 * zoomRate}
+                  hoverLeft={-267 + (-34 * zoomRate)}
+                  hoverTop={19 + (4 * zoomRate)}
+                  zoomRate={zoom}
                   onClick={handleOpenSpaceship}
-                  isBottom
                 />
-              </div> */}
+              </div>
             </div>
           </div>
         </div>
       </main>
+      <div className="fixed right-8 bottom-8 flex flex-col z-50" style={{}}>
+        <button
+          className="p-2 relative z-20 hover:bg-[#e1e4cd1a] active:bg-[#1E191566]"
+          onClick={zoomIn}
+        >
+          <PlusIcon />
+        </button>
+        <div className="bg-[#E1E4CD1A] w-4 h-[1px] z-20 ml-2" />
+        <button
+          className="p-2 relative z-20 hover:bg-[#e1e4cd1a] active:bg-[#1E191566]"
+          onClick={zoomOut}
+        >
+          <MinusIcon />
+        </button>
+        <div
+          className="absolute left-0 top-0 w-full h-full"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(15, 9, 2, 0.70) 0%, rgba(38, 33, 30, 0.70) 100%)",
+          }}
+        ></div>
+      </div>
       <Loading />
+
     </>
-  );
+  )
 };
 
 export default Map;
