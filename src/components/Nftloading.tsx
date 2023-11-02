2a0efd3a1509dc8807e6683017148437ca208432
supernoob1012 <felixmartel.no@gmail.com>
2023-11-03 02:54:53 +0900

    Update loding style

--- src/components/Nftloading.tsx
+++ src/components/Nftloading.tsx
@@ -1,12 +1,7 @@
 export const NftLoading = () => {
   return (
     <div className="w-52 h-16 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="shadow"></div>
-      <div className="shadow"></div>
-      <div className="shadow"></div>
+      <div className="one"></div>
     </div>
   );
 };
