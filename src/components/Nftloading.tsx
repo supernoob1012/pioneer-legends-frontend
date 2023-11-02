106ad4363f1f5bf40744e6309b1f3e4462c49390
supernoob1012 <felixmartel.no@gmail.com>
2023-11-03 03:19:13 +0900

    Update loading position

--- src/components/Nftloading.tsx
+++ src/components/Nftloading.tsx
@@ -1,6 +1,6 @@
 export const NftLoading = () => {
   return (
-    <div className="w-52 h-16 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="w-52 h-16 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
       <div className="one"></div>
     </div>
   );
