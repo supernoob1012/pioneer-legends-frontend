fd2a7f0c71f6de9ba458ea1e1d31da51834e23b6
supernoob1012 <felixmartel.no@gmail.com>
2023-11-03 18:42:23 +0000

    fix: admin addr

--- src/solana/constant.ts
+++ src/solana/constant.ts
@@ -8,4 +8,4 @@ export const USER_POOL_SEED = "user-stake-pool";
 
 export const PROGRAM_ID = new PublicKey("PLSeoaC7uLWpkbFjUMvF2Er4RXQThdA6T7S9ZY6BqiT");
 
-export const ADMIN_ADDRESS = new PublicKey("8GKyeeFosZcHbscZnfezt1yL3sVz7CuiXg1756RdSkDo");
+export const ADMIN_ADDRESS = new PublicKey("J9ja5QkewwMi9kG6JkCNxfLK9CoDGk3F4hZTNKQaKZe3");
